71199fb4bfb83f64e8dcc4c22a70a8c5482c899b
kolesan <kolesan90@gmail.com>
2019-03-22 10:44:15 +0200

    Add typeScript

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './style.css';
-
-console.log("Hello, World!");
-
-export function sum(a, b) {
-  return a + b;
-}
--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
   plugins: [new HtmlWebpackPlugin({
     template: './src/index.html'
   })],
@@ -14,8 +14,16 @@ module.exports = {
           'style-loader',
           'css-loader'
         ]
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
       }
     ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
   },
   output: {
     filename: 'bundle.js',
