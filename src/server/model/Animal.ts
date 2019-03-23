470694738cc2f0c38326dc7b3f8b47f5b646e4e0
kolesan <kolesan90@gmail.com>
2019-03-23 14:58:03 +0200

    Minor fix

--- src/server/model/Animal.ts
+++ src/server/model/Animal.ts
@@ -1,3 +1,12 @@
+export interface Animal {
+  name: string,
+  growth: number,
+  wiki: string,
+  carnivore: boolean,
+  height: number,
+  origin: string
+}
+
 export interface Property {
   readonly type: string,
   readonly sortable: boolean,
@@ -9,12 +18,3 @@ export const AnimalProperties: Map<string, Property> = new Map()
   .set("growth", { type: "number", sortable: true, filterable: false })
   .set("height", { type: "number", sortable: true, filterable: false })
   .set("origin", { type: "string", sortable: true, filterable: true });
-
-export interface Animal {
-  name: string,
-  growth: number,
-  wiki: string,
-  carnivore: boolean,
-  height: number,
-  origin: string
-}
