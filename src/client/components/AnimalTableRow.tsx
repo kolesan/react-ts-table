75bfa3a69c8f3634b545cd1cf7f5a1ca01a57277
kolesan <kolesan90@gmail.com>
2019-03-26 20:57:57 +0200

    Determine Growth cell color by it's value

--- src/client/components/AnimalTableRow.tsx
+++ src/client/components/AnimalTableRow.tsx
@@ -1,16 +1,29 @@
 import * as React from 'react';
 import TableCell from "@material-ui/core/TableCell";
 import TableRow from "@material-ui/core/TableRow";
+import withStyles from "@material-ui/core/styles/withStyles";
 
 export default function AnimalTableRow(props) {
   let { animal } = props;
+  let growth = animal.growth;
+  let GrowthCell = cellWithColorByValue(growth);
   return (
     <TableRow>
       <TableCell>{animal.name}</TableCell>
       <TableCell>{animal.origin}</TableCell>
-      <TableCell align="right">{animal.growth}</TableCell>
+      <GrowthCell align="right">{growth}</GrowthCell>
       <TableCell>{animal.carnivore ? "Yes" : "No"}</TableCell>
-      <TableCell align="right">{animal.height}</TableCell>
+      <TableCell align="right">{animal.height.toFixed(2)}</TableCell>
     </TableRow>
   );
+}
+
+function cellWithColorByValue(v) {
+  let ColoredCell = withStyles({
+    body: {
+      color: v > 0 ? "blue" : "red",
+    }
+  })(TableCell);
+
+  return v == 0 ? TableCell : ColoredCell;
 }
