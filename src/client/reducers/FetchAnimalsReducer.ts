66a3f666f9387ecfef23ff43b1521f2cd8704136
kolesan <kolesan90@gmail.com>
2019-03-27 21:55:42 +0200

    remove log

--- src/client/reducers/FetchAnimalsReducer.ts
+++ src/client/reducers/FetchAnimalsReducer.ts
@@ -4,7 +4,6 @@ import AnimalsResponse from "../model/AnimalsResponse";
 
 export default function fetchAnimalsReducer(state: AnimalsResponse = { total: 0, animals: [] }, action) {
   if (action.type == FETCH_ANIMALS) {
-    console.log("Received animals:", action.payload.data);
     return action.payload.data;
   }
   return state;
